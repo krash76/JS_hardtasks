3668611492fb89770ccd14fc2ee51bb9b6129a45
krash76 <krash76@mail.ru>
2022-02-11 11:53:09 +0100

    script.js changed

--- hw09/script.js
+++ hw09/script.js
@@ -19,12 +19,13 @@ const divs = document.querySelectorAll("div");
 const div1 = divs[0];
 const div2 = divs[1];
 
+
+
 setInterval( () => {
   let today = new Date();
   let weekday = rusWeek[today.getUTCDay()];
   let date = today.getDate();
   let month = today.getMonth() +1;
-  console.log (month);
   let monthText = rusMonths[today.getMonth()];
   let year = today.getFullYear();
   let hours = today.getHours();
@@ -33,32 +34,25 @@ setInterval( () => {
   let hoursForm;
   let minForm;
   let secForm;
-  
+  let minString = minutes.toString();
+  let secString = seconds.toString();
+   
+  const changeForm = function(el) {
+    if (el[el.length-1] === "1") {
+      el === minString ? minForm = "минута" : secForm = "секунда";
+    } else if ((el[el.length-1] === "2" || el[el.length-1] === "3" || el[el.length-1] === "4")) {
+      el === minString ? minForm = "минуты" : secForm = "секунды";
+    } else {
+      el === minString ? minForm = "минут": secForm = "секунд";
+    };
+  } 
+
   if (hours === 1 || hours === 21) {
     hoursForm = "час"
   } else if ((hours > 1 && hours < 5) || (hours > 21 && hours < 25)) {
     hoursForm = "часа";
   } else {
     hoursForm = "часов";
-  };
-
-  let minString = minutes.toString();
-  let secString = seconds.toString();
-  
-  if (minString[minString.length-1] === "1") {
-    minForm = "минута";
-  } else if ((minString[minString.length-1] === "2" || minString[minString.length-1] === "3" || minString[minString.length-1] === "4")) {
-    minForm = "минуты";
-  } else {
-    minForm = "минут";
-  };
-
-  if (secString[secString.length-1] === "1") {
-    secForm = "секунда";
-  } else if ((secString[secString.length-1] === "2" || secString[secString.length-1] === "3" || secString[secString.length-1] === "4")) {
-    secForm = "секунды";
-  } else {
-    secForm = "секунд";
   };
 
   if (date.toString().length < 2) {
@@ -76,6 +70,9 @@ setInterval( () => {
   if (seconds.toString().length < 2) {
     seconds = "0" + seconds.toString();
   };
+
+  changeForm(minString);
+  changeForm(secString);
   
   div1.textContent = `Сегодня ${weekday}, ${date} ${monthText} ${year} года, ${hours} ${hoursForm} ${minutes} ${minForm} ${seconds} ${secForm}`;
   div2.textContent = `${date}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
